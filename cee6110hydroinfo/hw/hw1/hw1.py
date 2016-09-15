34fbc71d9649e96adb0edfc216ff1afa77e0a2a0
Karun Joseph <karunjoseph@Karuns-MacBook-Pro.local>
2016-09-15 11:54:39 -0600

    hydroinfo hw1 done

---
+++ cee6110hydroinfo/hw/hw1/hw1.py
@@ -0,0 +1,27 @@
+##Plotting pH and dissolved oxygen levels
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+#csv file contains logan river data for 2015 from water lab site
+loganriverdata = pd.read_csv('iUTAH_GAMUT_LR_WaterLab_AA_RawData_2015.csv', skiprows=68)
+
+#plotting ph
+plt.plot(loganriverdata['pH'])
+plt.ylabel('ph')
+plt.xlabel('Time units [every 15 min]')
+plt.title('pH levels, 2015')
+plt.show()
+
+#plotting dissolved oxygen
+plt.plot(loganriverdata['ODO'])
+plt.ylabel('Dissolved oxgen [mg/L]')
+plt.xlabel('Time units [every 15 min]')
+plt.title('Dissolved oxygen levels, 2015')
+plt.show()
+
+
+
+
+
