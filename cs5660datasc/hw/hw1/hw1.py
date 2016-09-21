1395585cac2bec52327099def22e0804a75d6b37
Karun Joseph <karunjoseph@Karuns-MacBook-Pro.local>
2016-09-21 09:15:18 -0600

    cs5665 hw1 top5 done

--- cs5660datasc/hw/hw1/hw1.py
+++ cs5660datasc/hw/hw1/hw1.py
@@ -1,7 +1,10 @@
 """
 Questions - 9/14
 	1. Best way to deal with NaN for water consumption: ignore / replace with mean of all water values/ others?
-	2. What is the upperbound for water consumption?
+	2. What is the upperbound, lowerbound for water consumption? q1-q3?
+TODO - 
+	1. Ignoring outliers
+	2. Q3
 """
 
 import pandas as pd
@@ -14,8 +17,9 @@ csdf = pd.read_csv('CAStateBuildingMetrics.csv')
 ####1. Water usage analysis####
 ###############################
 
-#Preprocessing - ignoring NaN values. Mostly incorrect, may have to replace NaN with the mean?
-csdfclean = csdf['Water Use (All Water Sources) (kgal)'].dropna()
+#Preprocessing - Replacing NaN with 0 / mean
+#csdfclean = csdf['Water Use (All Water Sources) (kgal)'].dropna()
+csdfclean = csdf['Water Use (All Water Sources) (kgal)'].fillna(csdf['Water Use (All Water Sources) (kgal)'].mean())
 
 #Mean, Median, mode of all data
 print "Mean with outliers", csdfclean.mean()
@@ -29,7 +33,6 @@ for index, row in csdf.iterrows():
 		uniquedept[row['Department']]+=1
 	else:
 		uniquedept[row['Department']]=1 
-
 top5dept = sorted(uniquedept.items(), key=lambda x:x[1], reverse = True)[0:5]
 
 #Box plot for all buldings
@@ -37,25 +40,19 @@ plt.boxplot(csdfclean)
 plt.show()
 
 #Box plots for top 5 dept
-#csdf_top5=pd.DataFrame(columns=[dept[0] for dept in top5dept])
-csdf_top5=[]
+csdf_top5=pd.DataFrame(columns=[dept[0] for dept in top5dept])
 
 for index, row in csdf.iterrows():
 	if row.Department in [dept[0] for dept in top5dept]:
-		print "top 5 building!"
-		print dept[0]
-		#TODO: how to store above info for plotting? Tried data frame with 5 columns, after each loop add. Now trying tuples in a list
-		#csdf_top5.append({'DPR':6}, ignore_index=True)
+		#Poor approach. Can be more efficient
+		csdf_top5=csdf_top5.append({row['Department']:row['Water Use (All Water Sources) (kgal)']}, ignore_index=True)
 
-	else:
-		print "not a top5 building!"
-		
-# plt.figure()
-# plt.boxplot(csdf_top5)
-# plt.show()
+plt.boxplot([csdf_top5['CAL TRANS'].dropna(), csdf_top5['CAL FIRE'].dropna(), csdf_top5['DPR'].dropna(), csdf_top5['CHP'].dropna(), csdf_top5['CMD'].dropna()])
+plt.show()
 
 #Ignoring outliers, preprocessing
-# TODO: csdfcleanno
+q3=csdfclean.quantile(0.75)
+q1=csdfclean.quantile(0.25)
 
 # Mean, Median, mode of datawithout outiers
 # print "Mean without outliers", csdfcleanno.mean()
@@ -92,5 +89,16 @@ print "Persons correlation bw electricitiy and water usage ", csdf[['Water Use (
 # ####3. Building similarities####
 # ################################
 
+#List of attributes
+list(csdf.columns.values)
 
+#Slice of row whose property name matches query
+mendota_main_st = csdf.loc[csdf['Property Name']=='MENDOTA MAINTENANCE STATION']
+metro_state_hosp = csdf.loc[csdf['Property Name']=='METROPOLITAN STATE HOSPITAL']
+long_beach_foffice = csdf.loc[csdf['Property Name']=='LONG BEACH FIELD OFFICE']
+
+#Variabes of interest include 
+#Resource usage: electricity use, natural gas use, propane use, water use, site energy use
+#Property variables: dept name, city, primary property type, area
+#Both together
 
