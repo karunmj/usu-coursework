a5504faf0df88c83f9cdefc0b709e3dff6239cc9
Karun Joseph <karun.joseph@aggiemail.usu.edu>
2016-09-28 11:00:32 -0600

    cee6400hw3 done

--- cee6400phyhydro/hw/hw3/hw3r.R
+++ cee6400phyhydro/hw/hw3/hw3r.R
@@ -18,15 +18,21 @@ for (i in 1:nrow(annppt)){
 cum_avg_abde = cumsum(avg_abde)
 cum_c = cumsum(annppt['C'])
 
-#Plot bw cum_avg_abde and cum_c
-plot(cum_avg_abde,cum_c[,1],xaxt='n')
+#Plot bw cum_a  vg_abde and cum_c
+plot(cum_avg_abde,cum_c[,1],xaxt='n', xlab='Cumulative avg pptn, gages ABDE', ylab = 'Cumulative pptn, gage C', main = 'Measured & Corrected')
 axis(side = 1, at=cum_avg_abde, labels = c("1970", "1971", "1972", "1973", "1974", "1975", "1976", "1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986"))
 
 #Calculating fit, k value (assuming bw 1970-1974 and 1975-1986)
 fitpre = lm(cum_c[,1][1:5]~cum_avg_abde[1:5]) 
 fitpost = lm(cum_c[,1][6:17]~cum_avg_abde[6:17])
-abline(fitpre)
-abline(fitpost)
+
+#Plot best fit curves
+abline(fitpre, col='black')
+text(2800, 5500, "Pre slope=0.943", col = "black")
+arrows(x0=2500, y0=5000, x1=2000, y1=2000, col='black', length=0.1, lwd=2)
+abline(fitpost, col='black')
+text(10500, 14000, "Post slope=0.865", col = "black")
+arrows(x0=11000, y0=13500, x1=14000, y1=13000, col='black', length=0.1, lwd=2)
 sloppre = summary(fitpre$coefficients)[6]
 sloppost = summary(fitpost$coefficients)[1]
 k=sloppost/sloppre
@@ -34,8 +40,9 @@ k=sloppost/sloppre
 #Multiplying 1970-1974 C data with k
 Cnew=c(C[1:5]*k,C[6:17])
 
-  
-##2. Number of gauges question
+#Plot adjusted values
+lines(cum_avg_abde, cumsum(Cnew),type="o", pch=22, col='red')
+legend("topleft", cex=0.8, c("Measured values","Corrected values"), col=c("black","red"),lwd = c(4,4))
 
 
 ##3. Depth duration to Intensity duration frequency problem
@@ -55,33 +62,43 @@ twofourhrint = twofourhr/24
 #Plot intensity vs exceedence probability perc
 epp=ep*100
 
-plot(epp,onehrint, log ="xy")
+plot(epp,onehrint, log ="xy", xlab = 'Exceedance probability (%)', ylab = 'Intensity (in/hr)',main = 'Logarithmic probability plot of intensity of 1-h rainfall ')
 fitonehrint = lm((onehrint)~(epp)) 
 abline(fitonehrint,untf=TRUE)
 abline(v=4,col="green")
 abline(v=10,col="red")
 abline(v=20,col="blue")
 abline(v=50,col="yellow")
+legend("topright", cex=0.8, c("25-yr events","10-yr events", "5-yr events", "2-yr events"), col=c("green","red", "blue", "yellow"),lwd = c(4,4))
 onecol=predict(fitonehrint,newdata = data.frame(epp=c(50,20,10,4)))
 
-plot(epp,sixhrint,log = "xy")
+plot(epp,sixhrint,log = "xy",xlab = 'Exceedance probability (%)', ylab = 'Intensity (in/hr)',main = 'Logarithmic probability plot of intensity of 6-h rainfall ')
 fitsixhrint = lm((sixhrint)~(epp)) 
 abline(fitsixhrint, untf = TRUE)
 abline(v=4,col="green")
 abline(v=10,col="red")
 abline(v=20,col="blue")
 abline(v=50,col="yellow")
+legend("topright", cex=0.8, c("25-yr events","10-yr events", "5-yr events", "2-yr events"), col=c("green","red", "blue", "yellow"),lwd = c(4,4))
 sixcol=predict(fitsixhrint,newdata = data.frame(epp=c(50,20,10,4)))
 
-plot(epp, twofourhrint,log = "xy")
+plot(epp, twofourhrint,log = "xy",xlab = 'Exceedance probability (%)', ylab = 'Intensity (in/hr)',main = 'Logarithmic probability plot of intensity of 24-h rainfall ')
 fittwofourhrint = lm((twofourhrint)~(epp)) 
 abline(fittwofourhrint, untf = TRUE)
 abline(v=4,col="green")
 abline(v=10,col="red")
 abline(v=20,col="blue")
 abline(v=50,col="yellow")
+legend("topright", cex=0.8, c("25-yr events","10-yr events", "5-yr events", "2-yr events"), col=c("green","red", "blue", "yellow"),lwd = c(4,4))
 twofourcol=predict(fittwofourhrint,newdata = data.frame(epp=c(50,20,10,4)))
 
 #Plot intensity duration frequency
 rp = c(2, 5, 10, 25)
 intdurfreq = data.frame(rp,onecol,sixcol,twofourcol)
+
+plot(c(1,6,24),intdurfreq[1,][2:4],log='xy', xlab = 'Duration (hr)', ylab = 'Intensity (in/hr)',main = 'Intensity-duration-frequnecy plot for Chicago airport for durations from 1 to 24 hr and return periods from 2 to 25 yr', col = 'black', type = 'l')
+lines(c(1,6,24),intdurfreq[2,][2:4],log='xy', col = 'green')
+lines(c(1,6,24),intdurfreq[3,][2:4],log='xy', col = 'red')
+lines(c(1,6,24),intdurfreq[4,][2:4],log='xy', col = 'blue')
+legend("bottomleft", cex=0.8, c("2-yr","5-yr", "10-yr", "25-yr"), col=c("black","green", "red", "blue"),lwd = c(4,4))
+
